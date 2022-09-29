cac7d7c1b4a9ff10b2624bafef7083d8fbede74a
N Z <nelectrik@gmail.com>
2022-09-29 23:33:33 +0930

    Module 5 solution added, module 6 added

--- 05-Third-Party-APIs/01-Activities/26-Stu_Moment-Format/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/26-Stu_Moment-Format/Unsolved/assets/js/script.js
@@ -1,18 +1,18 @@
 // Use Moment.js to format the date and assign to the declared variable.
 // TODO: 1. What is your graduation date in the following format: Jan 1st, 1999?
-var gradDate;
+var gradDate = moment('01-08-2020', 'DD-MM-YY').format('MMM Do YYYY-MM-DD');
 $("#1a").text(gradDate);
 
 // TODO: 2. What day of the week will 1/1/2022 be?
-var weekDay;
+var weekDay = moment('1/1/2022', 'D/M/YYYY').format('dddd');
 $("#2a").text(weekDay);
 
 // TODO: 3. Out of 365, what day of the year is today?
-var now;
+var now = moment().format('DDDD');
 $("#3a").text(now);
 
 // TODO: 4. What is the current time in the format: hours:minutes:seconds
-var time;
+var time = moment().format();
 $("#4a").text(time);
 
 // TODO: 5. What is the current Unix timestamp?
