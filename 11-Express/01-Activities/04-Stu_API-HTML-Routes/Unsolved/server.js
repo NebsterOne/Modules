0a1c2df0720c6d30285da017fbd34ca0d20dccc3
N Z <nelectrik@gmail.com>
2022-11-08 21:44:24 +1030

    Extra content added, 11-Express

---
+++ 11-Express/01-Activities/04-Stu_API-HTML-Routes/Unsolved/server.js
@@ -0,0 +1,21 @@
+// TODO: Import express
+const express = require('express');
+
+// TODO: Import 'terms.json' file
+const data = require('./terms.json');
+const PORT = 3001;
+
+
+// TODO: Initialize app variable
+const app = express();
+
+// TODO: Create a route for a GET request that will return the content of our `terms.json` file
+app.get('/', (req, res) => res.send('go to /api'));
+app.get('/api', (req, res) => res.json(data));
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
