d6d49b762a20c1741f9e7abea9c0c32eaddc2827
N Z <nelectrik@gmail.com>
2022-11-10 18:39:16 +1030

    Solution for Ch11 added

--- 11-Express/01-Activities/02-Stu_Setup/Solved/server.js
+++ 11-Express/01-Activities/02-Stu_Setup/Solved/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
-// TODO: Invoke app.use() and serve static files from the '/public' folder
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
@@ -12,11 +11,10 @@ app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 app.get('/send', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/send.html'))
 );
+
 app.get('/paths', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/paths.html'))
 );
-
-// TODO: Create a route that will serve up the `public/paths.html` page
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
--- 11-Express/01-Activities/04-Stu_API-HTML-Routes/Unsolved/server.js
+++ 11-Express/01-Activities/04-Stu_API-HTML-Routes/Unsolved/server.js
@@ -1,20 +1,16 @@
 // TODO: Import express
 const express = require('express');
 
-// TODO: Import 'terms.json' file
 const data = require('./terms.json');
+// TODO: Import 'terms.json' file
 const PORT = 3001;
-
 
 // TODO: Initialize app variable
 const app = express();
 
 // TODO: Create a route for a GET request that will return the content of our `terms.json` file
-app.get('/', (req, res) => res.send('go to /api'));
-app.get('/api', (req, res) => res.json(data));
-
-
-
+app.get('/',(req,res) => res.send('go to /api'));
+app.get('/api',(req,res) => res.json(data));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
