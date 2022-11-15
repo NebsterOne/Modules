e8b29cb398b31ee922355c3adc6949bc20d2443b
N Z <nelectrik@gmail.com>
2022-11-15 18:23:35 +1030

    New files added

--- 11-Express/01-Activities/14-Stu_POST-Requests/Unsolved/server.js
+++ 11-Express/01-Activities/14-Stu_POST-Requests/Unsolved/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Create a GET method for `/api/reviews` that logs when a user's request has been received
 app.get('/api/reviews', (req, res) => {
-  // Your code here
+  res.json(`${req.method} request received`);
 });
 
 // TODO: Create a POST request for `/api/reviews` that logs when a user's request has been received
@@ -15,10 +15,14 @@ app.get('/api/reviews', (req, res) => {
 // TODO: Create a GET request for `api/upvotes` that logs when a user's request has been received
 app.get('/api/upvotes', (req, res) => {
   // Your code here
+  res.json(`${req.method} request received`);
 });
 
 // TODO: Create a POST request for `api/upvotes` that logs when a user's request has been received
 // Your code here
+app.get('/api/upvotes', (req, res) => {
+  res.json(`${req.method} request received`);
+});
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
